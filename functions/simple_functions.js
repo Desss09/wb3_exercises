a4ad7f75eb152d024552bf6c3d8e55b41c045285
Destiny <destinygil0926@gmail.com>
2024-04-30 13:04:51 -0500

    complted simple_functions

---
+++ functions/simple_functions.js
@@ -0,0 +1,30 @@
+"use strict"
+
+//fucntion to display my name, Favorite movie and favorite band
+function favoriteThings() {
+
+    console.log("My name is Destiny");
+    console.log("My favorite movie is Norbit");
+    console.log("My favorite band is Mindless Behavior");
+
+
+}
+
+
+
+
+
+//calling/invoking our functions
+favoriteThings();
+whyImHere();
+favoritePlace  ();
+
+function whyImHere() {
+
+    console.log("Im here in the program due to wanting to have a career that will always be needed and work life balance.")
+}
+
+function favoritePlace () {
+
+    console.log("My favorite place is anywhere away from the US")
+}
